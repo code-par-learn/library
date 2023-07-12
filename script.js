829963da3b971fb1dbcfd24fca0619187a1f8c9f
code-par-learn <117098183+code-par-learn@users.noreply.github.com>
2023-07-12 15:35:00 +0530

    changed the objects into class

--- script.js
+++ script.js
@@ -1,29 +1,27 @@
-let library=[];
-
-let btitle,bauthor,bpages,bread;
-let idt=document.getElementById('title');
-let ida=document.getElementById('author');
-let idp=document.getElementById('pages');
-let displaydivbk=document.getElementById('displaybkdiv');
-let displaydiv=document.getElementById('displaybk');
-let formdiv=document.getElementById('formdiv');
-document.addEventListener("keydown", (event) => {if(event.key=="Enter"){event.preventDefault()}})
-//document.getElementById('submit').addEventListener("click",(e)=>{e.preventDefault()});
 document.getElementById('submit').addEventListener("click",()=>{submit()});
-function Books(title,author,pages,read){
-        this.title=title;
-        this.author=author;
-        this.pages=pages;
-        this.read=read?"Read":"Not read";
-        
-    }
+document.addEventListener("keydown", (event) => {if(event.key=="Enter"){event.preventDefault()}})
+let library=[];
+class Library{
+  
+constructor(title,author,pages,read){
+    this.title=title;
+    this.author=author;
+    this.pages=pages;
+    this.read=read?"Read":"Not read";    
+    
+    
+}
 
-function addBooktoLibrary(bookobj){
-    let idx=library.push(bookobj);
-    displaybook(idx-1);
+
+ addBooktoLibrary(){
+  
+    let idx=library.push({"title":this.title,"author":this.author,"pages":this.pages,"read":this.read});
+    return idx-1;
+    
 }    
-function displaybook(idx){
-
+ displaybook(idx){
+    let displaydiv=document.getElementById('displaybk');
+   
         displaydiv.innerHTML+=`<div id="booksdiv${idx}"></div>`;
         let booksdiv=document.getElementById(`booksdiv${idx}`);
         booksdiv.innerHTML+=`<p>${library[idx].title}</p>`;
@@ -33,12 +31,18 @@ function displaybook(idx){
     
     
 }
+
+ 
+}
 function newBook(){
+    let formdiv=document.getElementById('formdiv');
     formdiv.style.display="flex";
     formdiv.style.flexDirection="row";
 }
-
-function submit(){
+ function submit(){
+    let idt=document.getElementById('title');
+    let ida=document.getElementById('author');
+    let idp=document.getElementById('pages');
     if(idt.value){
         if(ida.value){
             if(idp.value){
@@ -55,6 +59,10 @@ function submit(){
     else{
         return;
     }
+    let btitle,bauthor,bpages,bread;
+    
+    let displaydivbk=document.getElementById('displaybkdiv');
+
     btitle=idt.value;
     bauthor=ida.value;
     bpages=idp.value;
@@ -65,7 +73,7 @@ function submit(){
     ida.value='';
     idp.value='';
     displaydivbk.style.display="flex";
-    let bok=new Books(btitle,bauthor,bpages,bread);
-    addBooktoLibrary(bok);
+    let bok=new Library(btitle,bauthor,bpages,bread);
+    index1=bok.addBooktoLibrary();
+    bok.displaybook(index1);
 }
-
